0c286762e12827836d29bc6baa71f9840ccf75b7
Aravinthan333 <aravinthan2606@gmail.com>
2024-09-06 01:15:10 +0530

    first commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light p-2'>
+            <h6 className='text-center'>Athithyan B</h6>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+
+const Login = () => {
+  const [loading, setLaoding] = useState(false);
+  const navigate = useNavigate();
+  //Form Submit
+  const submitHandler = async (values) => {
+    try {
+      setLaoding(true);
+      const { data } = await axios.post(
+        "http://localhost:8080/api/v1/users/login",
+        values
+      );
+      setLaoding(false);
+      message.success("Login Success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: " " })
+      );
+      navigate("/");
+    } catch (error) {
+      setLaoding(false);
+      message.error("Invalid Something Went Wrong");
+    }
+  };
+
+  //Prevent for login user
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      <div className="login-loading">{loading && <Spinner />}</div>
+      <div className="login-content">
+        <div className="imagecontainer">
+          <img src={"../assets/Enpense1.1.png"} />
+        </div>
+        <div className="login-page">
+          <Form layout="vertical" onFinish={submitHandler}>
+            <h1>Login Form</h1>
+            <Form.Item label="Email" name="email">
+              <Input type="email" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input type="password" />
+            </Form.Item>
+            <div className="d-flex justify-content-between">
+              <Link to="/register">
+                Not A User? <br></br>Click Here To Register!
+              </Link>
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
